4357d5454093ce4df4a331353992c31ffea9006b
Terry Kerr <terry@idletea.net>
2025-12-26 15:17:47 -0500

    changes

---
+++ symlink/common/.config/nvim/lua/10-colorscheme.lua
@@ -0,0 +1,5 @@
+-- require("mini.deps").add { source = "rebelot/kanagawa.nvim" }
+-- vim.cmd [[:colorscheme kanagawa-dragon]]
+
+require("mini.deps").add { source = "webhooked/kanso.nvim" }
+vim.cmd [[:colorscheme kanso-zen]]
